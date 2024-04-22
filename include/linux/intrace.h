476de9828caf95ec883b85723aafc1d8f50215ce
yuvraj1803 <ysakshith@gmail.com>
2024-04-22 22:34:37 +0530

    removed intrace_init(void) prototype; added intrace_debugfs_file structure and entry for intrace/ directory inside debugfs root inside intrace_tracer struct.

--- include/linux/intrace.h
+++ include/linux/intrace.h
@@ -3,12 +3,13 @@
 
 #include <linux/types.h>
 #include <linux/spinlock.h>
+#include <linux/debugfs.h>
+#include <linux/fs.h>
 
 struct irq_domain;
 struct irq_desc;
 struct irq_common_data;
 
-void intrace_init(void);
 void intrace_buf_put(struct irq_domain*, struct irq_desc*);
 
 struct intrace_info{
@@ -20,6 +21,15 @@ struct intrace_tracer{
     struct intrace_info*     buff;
     unsigned long long       ptr;
     spinlock_t               lock;
+    struct dentry*           dir;   // dir in debugfs.
+};
+
+struct intrace_debugfs_file{
+    const char* name;
+    void* data;
+    umode_t mode;
+    const struct file_operations* fops;  
+    struct dentry*          file;   // file in debugfs under intrace_tracer->dir (parent)
 };
 
 #endif
