973764166c99ae67e55b08aade80242c8dc663d0
yuvraj1803 <ysakshith@gmail.com>
2024-04-22 22:36:43 +0530

    added intrace_debugfs_init() which initialises all files under intrace dir under debugfs root. We also define a macro INTRACE_DEBUGFS_FILE() that assists in defining intrace files in debugfs.

--- kernel/trace/intrace.c
+++ kernel/trace/intrace.c
@@ -2,18 +2,60 @@
 #include <linux/intrace.h>
 #include <linux/printk.h>
 #include <linux/types.h>
+#include <linux/debugfs.h>
 #include <linux/gfp_types.h>
 
-#define INTRACE_BUFFER_NR_PAGES     1
-#define INTRACE_BUFFER_RING_SIZE    (INTRACE_BUFFER_NR_PAGES * PAGE_SIZE)
-#define INTRACE_BUFFER_NR_ENTRIES   (INTRACE_BUFFER_RING_SIZE / sizeof(struct intrace_info))
+#define INTRACE_BUFFER_NR_PAGES              1
+#define INTRACE_BUFFER_RING_SIZE             (INTRACE_BUFFER_NR_PAGES * PAGE_SIZE)
+#define INTRACE_BUFFER_NR_ENTRIES            (INTRACE_BUFFER_RING_SIZE / sizeof(struct intrace_info))
+
+#define INTRACE_BUFFER_ADVANCE()                              (intracer->ptr = (intracer->ptr == INTRACE_BUFFER_NR_ENTRIES) ? 0 : intracer->ptr + 1)
+#define INTRACE_DEBUGFS_FILE(_name, _data, _mode, _fops)      {.name=(const char*)_name, .data=(void*)_data, .mode=(umode_t)_mode, .fops=(struct file_operations*)_fops, .file=(struct dentry*)NULL}
 
 static struct intrace_tracer* intracer;
 
-#define INTRACE_BUFFER_ADVANCE()    (intracer->ptr = (intracer->ptr == INTRACE_BUFFER_NR_ENTRIES) ? 0 : intracer->ptr + 1)
+static struct intrace_debugfs_file intrace_debugfs_files[] = {
+    INTRACE_DEBUGFS_FILE("testfile", 0, 0, 0)
+};
 
+static void __init intrace_debugfs_init(void){
 
-void intrace_init(void)
+    intracer->dir = debugfs_create_dir("intrace", NULL);
+    if(intracer->dir == ERR_PTR(-ENODEV)){
+        goto intrace_debugfs_fail;
+    }
+
+    for(int i = 0; i < ARRAY_SIZE(intrace_debugfs_files); i++){
+        struct dentry* file = debugfs_create_file(
+            intrace_debugfs_files[i].name,
+            intrace_debugfs_files[i].mode,
+            intracer->dir,  // parent directory in debugfs.
+            intrace_debugfs_files[i].data,
+            intrace_debugfs_files[i].fops
+        );
+
+        if(file == ERR_PTR(-ENODEV)){ // we free all the files and abort even if one file fails to get into debugfs.
+            pr_info("intrace: failed to create file intrace/%s under debugfs. debugfs seems to be disabled.\n", intrace_debugfs_files[i].name);
+            goto intrace_debugfs_free_everything;
+        }
+
+        intrace_debugfs_files[i].file = file;
+    }
+
+    goto out;
+
+intrace_debugfs_fail:
+    pr_info("intrace: Failed to create debugfs object. debugfs seems to be disabled.\n");
+
+intrace_debugfs_free_everything:
+    debugfs_remove_recursive(intracer->dir);    // if the dentry passed is NULL or an error pointer, nothing will be done. so no need to check the args against anything.
+
+out:
+    return;
+
+}
+
+static int __init intrace_init(void)
 {
 
     intracer = kzalloc(sizeof(*intracer), GFP_KERNEL);
@@ -30,6 +72,8 @@ void intrace_init(void)
 	    goto intrace_fail;
     }	    
 
+    intrace_debugfs_init();
+
 
     pr_info("intrace: Initialized intrace buffer.");
  
@@ -39,8 +83,10 @@ intrace_fail:
     pr_info("intrace: FAILED to allocate intrace buffer.");
 
 out:
-    return;
+    return 0;
 }
+
+late_initcall(intrace_init)
 
 void intrace_buf_put(struct irq_domain* domain, struct irq_desc* desc)
 {
@@ -57,5 +103,6 @@ void intrace_buf_put(struct irq_domain* domain, struct irq_desc* desc)
     INTRACE_BUFFER_ADVANCE();
 
 }
+
 
 
