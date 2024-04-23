e8c487fc1eb198cad5fafc833b55ab0a50ed66fc
yuvraj1803 <ysakshith@gmail.com>
2024-04-23 19:22:47 +0530

    removed redundant ramdisk.img

--- run.sh
+++
@@ -1,8 +0,0 @@
-qemu-system-aarch64	\
--nographic		\
--kernel arch/arm64/boot/Image	\
--append "console=ttyAMA0 nokaslr init=/sbin/init"	\
--cpu cortex-a72	-smp 4 \
--machine virt,virtualization=off	\
--initrd ramdisk.img	\
--m 1024
